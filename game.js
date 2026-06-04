4545375e75b1a8cec91786c506827570cbd386c6
nikunokuni <76274183+nikunokuni@users.noreply.github.com>
2026-06-04 14:47:20 +0900

    Update game.js

--- game.js
+++ game.js
@@ -551,12 +551,12 @@ ctx.lineCap = 'round';
 ctx.lineWidth = 2.5;
 
 ctx.beginPath(); // ★ここからパスを開始
-ctx.moveTo(-6, -43); 
+ctx.moveTo(-6, -55); 
 
 // ベジェ曲線で「豆の形」を一度に描く
 // 制御点を使って上側と下側のカーブを表現
-ctx.bezierCurveTo(-3,-40 , 3, -40, 6, -43); // 上側
-ctx.bezierCurveTo(3, -38, -3, -38, -6, -43); // 下側
+ctx.bezierCurveTo(-3,-53 , 3, -53, 6, -55); // 上側
+ctx.bezierCurveTo(3, -50, -3, -50, -6, -55); // 下側
 ctx.closePath(); // 始点と終点を閉じる
 ctx.stroke(); // ★最後に一度だけ描画する
 ctx.restore();
